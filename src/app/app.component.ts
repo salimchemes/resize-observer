38f828b34335270f4ea04972c864dfc9b88de213
Salim Chemes <salimchemes@gmail.com>
2021-05-16 19:18:08 -0300

    ResizeObserver for angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'resize-observer';
+  width: number = 0;
+  height: number = 0;
+  domRectReadOnly: DOMRectReadOnly | undefined;
+
+  onResize(entry: ResizeObserverEntry[]) {
+    this.width = entry[0].contentRect.width
+    this.height = entry[0].contentRect.height;
+    this.domRectReadOnly = entry[0].contentRect;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ResizeObserverModule } from '@ng-web-apis/resize-observer';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ResizeObserverModule
   ],
   providers: [],
   bootstrap: [AppComponent]
